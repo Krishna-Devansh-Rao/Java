694267e2e83e6c09f185779ec535cc7daed208e2
KRISHNA DEVANSH RAO <100765720+Krishna-Devansh-Rao@users.noreply.github.com>
2026-09-04 23:06:17 +0530

    Add files via upload

---
+++ Bro code/maain.java
@@ -0,0 +1,32 @@
+
+public class maain{
+    public static void main(String[] args) {
+
+//Baiscs
+// System.out.println("Hello World\t krr");
+// System.out.println("\"Hello World \"");
+// System.out.println("\\Hello World \\");
+
+//Variables
+// int x;
+// x= 18;
+// String y = "Virat Kohli";
+// System.out.println(x);
+// System.out.println("The Jersy no of "+y + " is " + x);
+        
+
+//Swap Two num
+// String x = "Water";
+// String y = "Oil";
+// String temp;
+// temp =x;
+// x = y;
+// y = temp;
+// System.out.println(x);
+// System.out.println(y);
+
+
+
+
+}
+}
---
+++ Bro code/main.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+        // This is my first java program
+
+        
+        // Introduction to Java
+        
+        //System.out.println("Krishna Here");
+        //System.out.print("I Love Cricket \n I am a Batsman \n I play Cricket \n");
+        //System.out.print("I play Cricket \n");
+    
+        
+        
+        //Variable
+
+        // 2 Steps to create a variable
+        // 1) Declaration
+        // 2) Assignment
+
+
+        //3)  Integer
+        // int x = 18;
+        // int y = 17;
+        // int z = 45;
+        // System.out.println("x + y + z = " + (x + y + z));
+        
+        // Double
+        
+        // double price = 9.2;
+        // double gps = 9.1;
+        // System.out.println(price) ;
+        // System.out.println(gps) ;
+        
+        // Char
+        // char grade = 'A';
+        // System.out.println(grade);
+        
+        // Boolean
+        // boolean student = true;
+        // System.out.println(student);
+        
+        // // If else
+        // int x = 18;
+        // int y = 17;
+        // if( x < y){
+        //     System.out.println("Virat Kohli is King");
+        // }
+        // else{
+        //     System.out.print("Virat KOhli is the only King");
+        // }
+
+        // String
+
+        // String Fname = "Krishna ";
+        // String Mname = "Devasnh ";
+        // String Lname = "Rao ";
+        // // System.out.print(Fname + Mname + Lname);
+
+        // String car =  "RollsRoyce " ;
+        // String colour = "White ";
+        // String location = "Shamrock Greens" ;
+        // System.out.print("Hey I am " + (Fname + Mname + Lname) + "and I have seen a " + (colour + car + "at " + location) );
+
+
+        // Input from user
+
+        System.out.print("Enter detail: ");
+        // Scanner name = new Scanner(System.in);
+        // String name = sc.nextLine();
+        // System.out.print(name);
+        // name.close();
+
+
+    }
+
+}
