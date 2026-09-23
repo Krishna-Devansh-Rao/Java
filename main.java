01d0eb960561a7ab4de2272c024acd8eaa95e774
KRISHNA DEVANSH RAO <100765720+Krishna-Devansh-Rao@users.noreply.github.com>
2026-09-23 23:55:22 +0530

    Update main.java

--- main.java
+++ main.java
@@ -2,6 +2,7 @@ public class main{
     public static void main(String[] arg){
         System.out.println("I Love pizza");
         System.out.println("I Love pizza");
+        System.out.println("I Love pizza");
     }
 }
 
